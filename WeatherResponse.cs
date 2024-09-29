277edddf62f15a82c5199b9ac68e896333fda314
R Smith <gatortarheel@gmail.com>
2024-09-29 14:46:24 -0400

    some null things updated

--- WeatherResponse.cs
+++ WeatherResponse.cs
@@ -33,18 +33,18 @@ public class Clouds
     public class WeatherResponse
 #pragma warning restore CA1050 // Declare types in namespaces
     {
-        public Coord coord { get; set; }
-        public List<Weather> weather { get; set; }
-        public string @base { get; set; }
-        public Main main { get; set; }
+        public Coord? coord { get; set; }
+        public List<Weather>? weather { get; set; }
+        public string? @base { get; set; }
+        public Main? main { get; set; }
         public int visibility { get; set; }
-        public Wind wind { get; set; }
-        public Clouds clouds { get; set; }
+        public Wind? wind { get; set; }
+        public Clouds? clouds { get; set; }
         public int dt { get; set; }
-        public Sys sys { get; set; }
+        public Sys? sys { get; set; }
         public int timezone { get; set; }
         public int id { get; set; }
-        public string name { get; set; }
+        public string? name { get; set; }
         public int cod { get; set; }
     }
 
@@ -54,7 +54,7 @@ public class Clouds
     {
         public int type { get; set; }
         public int id { get; set; }
-        public string country { get; set; }
+        public string? country { get; set; }
         public int sunrise { get; set; }
         public int sunset { get; set; }
     }
@@ -64,9 +64,9 @@ public class Clouds
 #pragma warning restore CA1050 // Declare types in namespaces
     {
         public int id { get; set; }
-        public string main { get; set; }
-        public string description { get; set; }
-        public string icon { get; set; }
+        public string? main { get; set; }
+        public string? description { get; set; }
+        public string? icon { get; set; }
     }
 
 #pragma warning disable CA1050 // Declare types in namespaces
